f3d8c93e24b34b0841a7a73b221c0d7820103467
Christopher Early <cearly@cs.uoregon.edu>
2017-05-31 16:17:10 -0700

    Clients can now form a ring topology and elect a leader. A newly entering client will cause the topology to be re-constructed, and will also trigger re-election.

--- Main.java
+++ Main.java
@@ -2,21 +2,27 @@ package com.kingfisher;
 
 import java.net.*;
 
-public class BBSClient {
+public class Main {
 
     public static void main(String[] args) throws Exception {
+
+        // First step is to get config info from file
+        // config file contains:
+        // beginning and end of port range
+        // port number for this client
+        // client's join time
+        // client's leave time
+
         int beginRange = 11100;
         int endRange = 11110;
-        int portRange = endRange - beginRange;
         int portNum = Integer.parseInt(args[0]);
-        //        int portNum = 11105;
-        int clientID = portNum - beginRange;
+        int portRange = endRange - beginRange;
+        int leaderPortNum = -1;
+
         byte[] sendData;
         byte[] receiveData = new byte[1024];
-        sendData = "probe".getBytes();
         DatagramSocket sock = new DatagramSocket(portNum);
         int defaultSoTimeout = sock.getSoTimeout();
-        sock.setSoTimeout(2000);
         InetAddress address = InetAddress.getByName("localhost");
 
         int nextHopPortNum = -1;
@@ -25,7 +31,7 @@ public class BBSClient {
 
         System.out.println("Forming ring...");
         // Loop until client has established next & prev hops
-        while(nextHopPortNum < 0 || prevHopPortNum < 0) {
+        while(true) {
             // First step of discovery phase
             // Step through port range and probe each address
 
@@ -34,10 +40,12 @@ public class BBSClient {
                 if(probePortNum > endRange) probePortNum -= portRange;
                 else if(probePortNum == portNum) probePortNum++;
                 //System.out.println("Sending to port " + probePortNum);
-                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, address, probePortNum);
-                sock.send(sendPacket);
+                sendData = "probe".getBytes();
+                sock.send(new DatagramPacket(sendData, sendData.length, address, probePortNum));
             }
             try {
+                if(nextHopPortNum > -1) sock.setSoTimeout(defaultSoTimeout);
+                else sock.setSoTimeout(2000);
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                 sock.receive(receivePacket);
                 String data = new String(receivePacket.getData());
@@ -45,38 +53,37 @@ public class BBSClient {
                     //System.out.println("got probe from " + receivePacket.getPort());
                     byte[] probeReplyData = "OKprobe".getBytes();
                     sock.send(new DatagramPacket(probeReplyData, probeReplyData.length, address, receivePacket.getPort()));
+                    if(prevHopPortNum > -1) {
+                        probeReplyData = "reconfig".getBytes();
+                        sock.send(new DatagramPacket(probeReplyData, probeReplyData.length, address, prevHopPortNum));
+                    }
                     prevHopPortNum = receivePacket.getPort();
                     System.out.println("Found prev hop! " + prevHopPortNum);
                 }
                 else if(data.startsWith("OKprobe")) {
                     nextHopPortNum = receivePacket.getPort();
                     System.out.println("Found next hop! " + nextHopPortNum);
+                    sendData = ("election," + portNum).getBytes();
+                    sock.send(new DatagramPacket(sendData, sendData.length, address, nextHopPortNum));
+                }
+                else if(data.startsWith("reconfig")) {
+                    nextHopPortNum = -1;
+                    probePortNum = portNum;
+                }
+                else if(data.startsWith("election")) {
+                    leaderPortNum = Integer.parseInt((data.substring(data.indexOf(",") + 1)).trim());
+                    if(nextHopPortNum > 0 && leaderPortNum != portNum) {
+                        if(leaderPortNum < portNum) sendData = ("election," + portNum).getBytes();
+                        else sendData = ("election," + leaderPortNum).getBytes();
+                        sock.send(new DatagramPacket(sendData, sendData.length, address, nextHopPortNum));
+                    }
+                    else if(leaderPortNum == portNum) {
+                        System.out.println("I am the leader");
+                    }
                 }
             } catch(SocketTimeoutException e) {
                 //if(nextHopPortNum < 0) System.out.println("No response.");
             }
         }
-        System.out.println("Ready!");
-        while(true) {
-            sock.setSoTimeout(defaultSoTimeout);
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            sock.receive(receivePacket);
-            String data = new String(receivePacket.getData());
-            if(data.startsWith("probe") && receivePacket.getPort() > prevHopPortNum) {
-                //System.out.println("got probe from " + receivePacket.getPort());
-                byte[] probeReplyData = "OKprobe".getBytes();
-                sock.send(new DatagramPacket(probeReplyData, probeReplyData.length, address, receivePacket.getPort()));
-                probeReplyData = "reconfig".getBytes();
-                sock.send(new DatagramPacket(probeReplyData, probeReplyData.length, address, prevHopPortNum));
-                prevHopPortNum = receivePacket.getPort();
-                System.out.println("New prev hop! " + prevHopPortNum);
-
-            }
-            else if(data.startsWith("OKprobe")) {
-                nextHopPortNum = receivePacket.getPort();
-                System.out.println("Found next hop! " + nextHopPortNum);
-            }
-        }
-
     }
 }
